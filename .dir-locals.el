4e034de250c134824e8f1d72c71517580384505f
Handol Park <handolpark12@gmail.com>
2018-09-26 03:48:59 +0000

    remove emacs directory local variables

--- .dir-locals.el
+++
@@ -1,7 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((python-mode
-  (pytest-global-name . "pipenv run python -m pytest")
-  (pyvenv-activate . ~/\.virtualenvs/ml-examples-6O9Scrxc)))
-
