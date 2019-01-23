fd0f8a59bcbc0b2bc916d5230f40f205bd7bf028
Handol Park <handolpark12@gmail.com>
2019-01-22 19:00:06 -0800

    incomplete

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((python-mode
-  (pytest-global-name . "pipenv run python -m pytest")
-  (pyvenv-activate . ~/\.virtualenvs/ml-examples-6O9Scrxc)))
-
+((nil . ())
+ (python-mode . ((pyvenv-workon . "dl"))))
